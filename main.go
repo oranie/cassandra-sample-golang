e589a711a9710e26f8b0b5fa358b5006a4a4f4e3
oranie <t.narita0917@gmail.com>
2019-12-03 12:06:07 +0900

    change to chat app

--- main.go
+++ main.go
@@ -20,13 +20,14 @@ type Chat struct {
 
 func main() {
 	// connect to the cluster
-	// local laptop need to connect ssh tunnel
+	// My envroiment : local laptop need to connect cassandra cluster with ssh tunnel
 	// example ssh ssh.host -L 9042:cassandra.host:9042
 
 	cluster := gocql.NewCluster("127.0.0.1")
 	cluster.Keyspace = "example"
 	cluster.Consistency = gocql.Quorum
-	cluster.CQLVersion = "5.0.1"
+	//cluster.CQLVersion = "5.0.1"
+
 	session, error := cluster.CreateSession()
 	if error != nil {
 		fmt.Println(error)
@@ -35,7 +36,7 @@ func main() {
 	defer session.Close()
 
 	// create table
-	println("Create chat table progress.......")
+	log.Println("Create chat table progress.......")
 	if err := session.Query(`CREATE TABLE IF NOT EXISTS chat (
 		name text,
 		time bigint,
@@ -45,37 +46,66 @@ func main() {
 		WITH CLUSTERING ORDER BY (time DESC);`).Exec(); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Create chat table done!")
 
-	println("Create chat table done!")
-
-	var name string
-	var time int64
-	var chat_room string
-	var comment string
-
+	//Insert test data
 	chatData := generateChatData()
 
+	insertTestData(session, &chatData)
+	selectTestData(session, &chatData)
+	allSelectTestData(session)
+
+}
+
+// Insert test data
+func insertTestData(session *gocql.Session, chatData *Chat) {
+	log.Println("Insert test data....")
 	if err := session.Query(`INSERT INTO chat (name,time,chat_room,comment) VALUES (?,?,?,?)`,
-		chatData.Name, chatData.Time, chatData.Chat_room, chatData.Comment).Exec(); err != nil {
+		chatData.Name,
+		chatData.Time,
+		chatData.Chat_room,
+		chatData.Comment).Exec(); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Insert test data done!")
+}
 
-	if err := session.Query(`SELECT * FROM chat where name = ?`,
-		chatData.Name).Consistency(gocql.One).Scan(&name, &time, &chat_room, &comment); err != nil {
+//Get Insert data
+func selectTestData(session *gocql.Session, chatData *Chat) {
+	log.Println("Select insert test data....")
+	var selectChatData Chat
+	if err := session.Query(`SELECT name,time,chat_room,comment FROM chat where name = ?`,
+		chatData.Name).Consistency(gocql.One).Scan(
+		&selectChatData.Name,
+		&selectChatData.Time,
+		&selectChatData.Chat_room,
+		&selectChatData.Comment); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Insert Chat:", name, time, chat_room, comment)
+	log.Println("Insert Data:", selectChatData)
+	log.Println("Select insert test data done!")
+}
 
+// list all chat
+func allSelectTestData(session *gocql.Session) {
 	// list all chat
+	log.Println("Select all table data...")
+	var selectAllChatData Chat
 	iter := session.Query(`SELECT name,time,chat_room,comment FROM chat`).Iter()
-	for iter.Scan(&name, &time, &chat_room, &comment) {
-		fmt.Println("All Chat:", name, time, chat_room, comment)
+	for iter.Scan(
+		&selectAllChatData.Name,
+		&selectAllChatData.Time,
+		&selectAllChatData.Chat_room,
+		&selectAllChatData.Comment) {
+		fmt.Println("All Chat:", selectAllChatData)
 	}
 	if err := iter.Close(); err != nil {
 		log.Fatal(err)
 	}
+	log.Println("Select all table data Done!")
 }
 
+// generate random test data
 func generateChatData() Chat {
 	now := time.Now()
 
@@ -89,6 +119,7 @@ func generateChatData() Chat {
 	return chatData
 }
 
+//generate random string: 4char
 func random() string {
 	var n uint64
 	binary.Read(rand.Reader, binary.LittleEndian, &n)
