64d579b216839feb8d710710e78a1843cd92063b
oranie <t.narita0917@gmail.com>
2019-12-05 16:20:10 +0900

    add /chat/cooments/latest and all

--- main.go
+++ main.go
@@ -51,6 +51,8 @@ func main() {
 		c.String(http.StatusOK, "Test done.", result)
 	})
 
+	r.StaticFile("/chat", "./web/livechat.html")
+
 	r.POST("/chat/comments/add", func(c *gin.Context) {
 		var json chat.Chat
 		if err := c.ShouldBindJSON(&json); err != nil {
